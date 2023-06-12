59d7636cc4df2a50e85b871a5b421614a64c984b
youngspeculator <jayypalll@gmail.com>
2023-06-12 01:55:48 -0700

    adding the LCD Backlights

--- web_scraper/WebScrapes.py
+++ web_scraper/WebScrapes.py
@@ -9,8 +9,8 @@ Product_Prices = []
 # Start the index from 1
 index = 1
 
-for page_number in range(1, 3):
-    url = f"https://www.f2parts.com/collection/realme-vivo?page={page_number}"
+for page_number in range(1, 4):
+    url = f"https://www.f2parts.com/collection/lcd-backlight?page={page_number}"
     source = requests.get(url).text
     soup = BeautifulSoup(source, 'html.parser')
 
@@ -33,7 +33,7 @@ for page_number in range(1, 3):
 data = list(zip(Product_Names, Product_Prices, Product_Images))
 
 # Writing the data to a CSV file
-filename = 'Realme-Vivo.csv'
+filename = 'LCD-backlights.csv'
 with open(filename, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['Product Name', 'Product Price', 'Product Image'])
