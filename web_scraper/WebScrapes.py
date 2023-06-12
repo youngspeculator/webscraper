4805843c315b207650012bc9163ebc4d3782100d
youngspeculator <jayypalll@gmail.com>
2023-06-12 09:46:14 -0700

    making the images visible

--- web_scraper/WebScrapes.py
+++ web_scraper/WebScrapes.py
@@ -2,20 +2,20 @@ import csv
 from bs4 import BeautifulSoup
 import requests
 
-List_A = []
-List_B = []
-List_C = []
+Product_Names = []
+Product_Prices = []
+Product_Images = []
 
 # Start the index from 1
 index = 1
 
-for page_number in range(,):
-    url = f"?page={page_number}"
+for page_number in range(1, 4):
+    url = f"https://www.f2parts.com/collection/hydrogel?page={page_number}"
     source = requests.get(url).text
     soup = BeautifulSoup(source, 'html.parser')
 
 
-    for name, price, image in zip(soup.findAll('a', class_='product-link'), soup.findAll('div', class_='price in-card'), soup.findAll('img', src_='')):
+    for name, price, image in zip(soup.findAll('a', class_='product-link'), soup.findAll('div', class_='price in-card'), soup.findAll('img')):
         Product_Name = name.get_text(strip=True)
         Product_Price = price.get_text(strip=True)
         Product_Image = image['src']
@@ -26,17 +26,17 @@ for page_number in range(,):
             Product_Images.append(Product_Image)
 
             # Add the index to the product information
-            Product_Names[-1] = f"{index}. {List_A [-1]}"
+            Product_Names[-1] = f"{index}. {Product_Names [-1]}"
             index += 1
 
 # Preparing the data for writing to CSV
 data = list(zip(Product_Names, Product_Prices, Product_Images))
 
 # Writing the data to a CSV file
-filename = '.csv'
+filename = 'Hydrogel.csv'
 with open(filename, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(['List A', 'List B', 'List C'])
+    writer.writerow(['Product_Names', 'Product_Prices', 'Product_Images'])
     writer.writerows(data)
 
 print(f"Data was scraped successfully and saved in {filename}")
