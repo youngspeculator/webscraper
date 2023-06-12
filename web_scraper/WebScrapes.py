8fa832dbaf5e0e630b241d6f6505a8bdbdb4c0ea
youngspeculator <jayypalll@gmail.com>
2023-06-12 02:18:34 -0700

    adding more assorted parts

--- web_scraper/WebScrapes.py
+++ web_scraper/WebScrapes.py
@@ -9,8 +9,8 @@ Product_Prices = []
 # Start the index from 1
 index = 1
 
-for page_number in range(1, 25):
-    url = f"https://www.f2parts.com/collection/sim-derzhateli?page={page_number}"
+for page_number in range(1, 20):
+    url = f"https://www.f2parts.com/collection/stikery?page={page_number}"
     source = requests.get(url).text
     soup = BeautifulSoup(source, 'html.parser')
 
@@ -33,7 +33,7 @@ for page_number in range(1, 25):
 data = list(zip(Product_Names, Product_Prices, Product_Images))
 
 # Writing the data to a CSV file
-filename = 'SIM_Holder.csv'
+filename = 'Stickers.csv'
 with open(filename, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['Product Name', 'Product Price', 'Product Image'])
